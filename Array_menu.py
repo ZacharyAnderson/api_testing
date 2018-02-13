c026dac148cd325ca3aeaf92ddc84ad8f6b095b1
Zach Anderson <zacha777@gmail.com>
2018-02-13 12:52:26 -0500

    Added requirements.txt and started testing auth token

--- Array_menu.py
+++ Array_menu.py
@@ -1,8 +1,14 @@
 import os, sys
+import requests
 
 # This Function will be used to authenticate into the ESGMID3PAR7400C1 API
 # and be interactive for specific types of programming tasks
 def ESGMID3PAR7400C1():
     print ('You made it into ESGMID3PAR7400C1\n')
     print ('Here we have the ability to complete basic storage administration tasks through the HPE 3PAR WSAPI.\n')
+   # headers = {'X-HP3PAR-WSAPI-SessionKey':''}
+    #payload = {''}
+
+   # r = requests.post('https://192.168.142.50:8080/api/v1/credentials', data = {'user':'3paradm', 'password':'bare4115.'}, headers = {'Accept':'application/json'}, verify=False)
+
     return
